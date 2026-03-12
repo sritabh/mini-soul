65cf37621f3b4eac76e0750ab671c490101de574
Sritabh Priyadarshi <sobydanny@gmail.com>
2026-03-12 23:27:13 +0530

    rtc util

--- main.py
+++ main.py
@@ -1,35 +1,10 @@
-from machine import SoftI2C, Pin
-from ds3231 import DS3231
+from machine import Pin
 from ssd1306 import SSD1306_I2C
 import time
 import urandom
+import rtc_utils
 
-i2c = SoftI2C(sda=Pin(8), scl=Pin(9))
-rtc = DS3231(i2c)
-oled = SSD1306_I2C(128, 64, i2c)
-
-EEPROM_ADDR = 0x57
-FLAG_ADDR = 0x0000
-FLAG_VALUE = 0xAB
-
-def eeprom_write(addr, value):
-    i2c.writeto(EEPROM_ADDR, bytes([addr >> 8, addr & 0xFF, value]))
-    time.sleep_ms(10)
-
-def eeprom_read(addr):
-    i2c.writeto(EEPROM_ADDR, bytes([addr >> 8, addr & 0xFF]))
-    return i2c.readfrom(EEPROM_ADDR, 1)[0]
-
-def is_time_set():
-    return eeprom_read(FLAG_ADDR) == FLAG_VALUE
-
-def mark_time_set():
-    eeprom_write(FLAG_ADDR, FLAG_VALUE)
-
-def init_rtc(force_set=False):
-    if force_set or not is_time_set():
-        rtc.set_datetime(2026, 3, 11, 22, 13, 0)
-        mark_time_set()
+oled = SSD1306_I2C(128, 64, rtc_utils.i2c)
 
 def draw_oval(cx, cy, rx, ry, col=1):
     for y in range(-ry, ry + 1):
@@ -53,16 +28,9 @@ def draw_eyes(blinking=False):
         draw_oval(lx, ly, 3, 4)
         draw_oval(rx, ry, 3, 4)
 
-def get_time_strings():
-    yy, mo, dd, hh, mm, ss = rtc.datetime()
-    period = "AM" if hh < 12 else "PM"
-    hh12 = hh % 12 or 12
-    time_str = "{:02d}:{:02d}:{:02d} {}".format(hh12, mm, ss, period)
-    date_str = "{:02d}/{:02d}/{:04d}".format(dd, mo, yy)
-    return time_str, date_str
 
 def draw_frame(blinking=False):
-    time_str, date_str = get_time_strings()
+    time_str, date_str = rtc_utils.get_time_strings()
     oled.fill(0)
     draw_eyes(blinking=blinking)
     oled.hline(0, 44, 128, 1)
@@ -76,7 +44,7 @@ def draw_frame(blinking=False):
     oled.show()
 
 # --- Boot ---
-init_rtc(force_set=False)
+rtc_utils.init_rtc(force_set=False, new_time=(2026, 3, 11, 22, 13, 0))
 
 # --- Main loop ---
 tick = 0
@@ -93,4 +61,3 @@ while True:
         draw_frame(blinking=False)
         tick = 0
         next_blink = urandom.getrandbits(4) + 3
-
---
+++ rtc_utils.py
@@ -0,0 +1,46 @@
+from machine import SoftI2C, Pin
+from ds3231 import DS3231
+import time
+
+EEPROM_ADDR = 0x57
+FLAG_ADDR = 0x0000
+FLAG_VALUE = 0xAB
+
+i2c = SoftI2C(sda=Pin(8), scl=Pin(9))
+rtc = DS3231(i2c)
+
+
+def eeprom_write(addr, value):
+    i2c.writeto(EEPROM_ADDR, bytes([addr >> 8, addr & 0xFF, value]))
+    time.sleep_ms(10)
+
+
+def eeprom_read(addr):
+    i2c.writeto(EEPROM_ADDR, bytes([addr >> 8, addr & 0xFF]))
+    return i2c.readfrom(EEPROM_ADDR, 1)[0]
+
+
+def is_time_set():
+    return eeprom_read(FLAG_ADDR) == FLAG_VALUE
+
+
+def mark_time_set():
+    eeprom_write(FLAG_ADDR, FLAG_VALUE)
+
+
+def init_rtc(force_set=False, new_time=None):
+    if force_set or not is_time_set():
+        if new_time:
+            rtc.set_datetime(*new_time)
+        else:
+            rtc.set_datetime(2026, 3, 11, 22, 13, 0)
+        mark_time_set()
+
+
+def get_time_strings():
+    yy, mo, dd, hh, mm, ss = rtc.datetime()
+    period = "AM" if hh < 12 else "PM"
+    hh12 = hh % 12 or 12
+    time_str = "{:02d}:{:02d}:{:02d} {}".format(hh12, mm, ss, period)
+    date_str = "{:02d}/{:02d}/{:04d}".format(dd, mo, yy)
+    return time_str, date_str
